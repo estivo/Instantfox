c0e905195b6021f84ed3804240b11ebe29d2a66f
Johan <johangit@mail.estivo.de>
2011-04-20 15:25:31 +0200

    simple poland + netherland localization + fixed suggest for other locales

--- plugins/firefox/chrome/locale/es-ES/plugins.js
+++ plugins/firefox/chrome/locale/es-ES/plugins.js
@@ -12,36 +12,60 @@
     yh:   'yahoo',
     b:    'bing',
     t:    'twitter',
-    e:    'ebay'
+    a:    'amazon',
+    e:    'ebay',
+    gg:		'googleluck',
+    wa:   'wolframalpha'
   };
   
   InstantFox.Plugins.extend({
     googleFrame: {
-      url: 'http://www.google.%ld/#q=%q&fp=1&cad=b'
+      url: 'http://www.google.%ld/#q=%q&fp=1&cad=b',
+      json: 'http://suggestqueries.google.com/complete/search?json&q=%q&hl=es-es'
     },
     googleImages: {
-      url: 'http://www.google.%ld/images?q=%q&hl=%ls'
+      url: 'http://www.google.%ld/images?q=%q&hl=%ls',
+      json: 'http://suggestqueries.google.com/complete/search?json&ds=i&q=%q'
     },
     googleMaps: {
-      url: 'http://maps.google.com/maps?q=%q'
+      url: 'http://maps.google.com/maps?q=%q',
+	  json:false //json: 'http://maps.google.de/maps/suggest?q=%q&cp=1&hl=de&gl=de&v=2&json=b' !leads into syntax error!
     },
     youTube: {
-      url: 'http://www.youtube.com/results?search_query=%q'
+      url: 'http://www.youtube.com/results?search_query=%q',
+	  json: 'http://suggestqueries.google.com/complete/search?json&ds=yt&q=%q'
+    },
+    googleluck: {
+      url: 'http://www.google.com/search?hl=%ls&q=%q&btnI=Auf+gut+Gl%C3%BCck!',
+  	  json: false
     },
     twitter: {
-      url: 'http://search.twitter.com/search?q=%q'
+      url: 'http://search.twitter.com/search?q=%q',
+   	  json: false
+    },
+    amazon: {
+      url: 'http://www.amazon.com/gp/search?ie=UTF8&keywords=%q&tag=406-20&index=aps&linkCode=ur2&camp=1789&creative=9325',
+	  json: 'http://completion.amazon.co.uk/search/complete?method=completion&q=%q&search-alias=aps&mkt=4'
     },
     ebay: {
-      url: 'http://shop.ebay.%ld/?_nkw=%q'
+      url: 'http://shop.ebay.%ld/?_nkw=%q',
+	  json: 'http://anywhere.ebay.com/services/suggest/?s=0&q=%q' // from firefoxes searchservice
     },
     yahoo: {
-      url: 'http://%ld.search.yahoo.com/search?p=%q&ei=UTF-8'
+      url: 'http://search.yahoo.com/search?p=%q&ei=UTF-8',
+	  json: false
     },
     bing: {
-      url: 'http://www.bing.com/search?q=%q&form=QBLH'
+      url: 'http://www.bing.com/search?q=%q&form=QBLH',
+	  json: 'http://api.bing.com/osjson.aspx?query=%q&form=OSDJAS'
     },
     wikipedia: {
-      url: 'http://%ls.wikipedia.org/w/index.php?search=%q'
+      url: 'http://es.wikipedia.org/w/index.php?search=%q',
+	  json: 'http://es.wikipedia.org/w/api.php?action=opensearch&search=%q'
+    },
+    wolframalpha: {
+      url: 'http://www.wolframalpha.com/input/?i=%q',
+	  json: false
     }
   });
   
--- plugins/firefox/chrome/locale/fr/plugins.js
+++ plugins/firefox/chrome/locale/fr/plugins.js
@@ -13,39 +13,59 @@
     b:    'bing',
     t:    'twitter',
     a:    'amazon',
-    e:    'ebay'
+    e:    'ebay',
+    gg:		'googleluck',
+    wa:   'wolframalpha'
   };
   
   InstantFox.Plugins.extend({
     googleFrame: {
-      url: 'http://www.google.%ld/#hl=%ls&q=%q&fp=1&cad=b'
+      url: 'http://www.google.%ld/#hl=%ls&q=%q&fp=1&cad=b',
+      json: 'http://suggestqueries.google.com/complete/search?json&q=%q&hl=fr'
     },
     googleImages: {
-      url: 'http://www.google.%ld/images?q=%q&hl=%ls'
+      url: 'http://www.google.%ld/images?q=%q&hl=%ls',
+      json: 'http://suggestqueries.google.com/complete/search?json&ds=i&q=%q'
     },
     googleMaps: {
-      url: 'http://maps.google.com/maps?q=%q'
+      url: 'http://maps.google.com/maps?q=%q',
+	  json:false //json: 'http://maps.google.de/maps/suggest?q=%q&cp=1&hl=de&gl=de&v=2&json=b' !leads into syntax error!
     },
     youTube: {
-      url: 'http://www.youtube.com/results?search_query=%q'
+      url: 'http://www.youtube.com/results?search_query=%q',
+	  json: 'http://suggestqueries.google.com/complete/search?json&ds=yt&q=%q'
+    },
+    googleluck: {
+      url: 'http://www.google.com/search?hl=%ls&q=%q&btnI=Auf+gut+Gl%C3%BCck!',
+  	  json: false
     },
     twitter: {
-      url: 'http://search.twitter.com/search?q=%q'
+      url: 'http://search.twitter.com/search?q=%q',
+   	  json: false
     },
     amazon: {
-      url: 'http://www.amazon.fr/gp/search?ie=UTF8&keywords=%q&tag=604-21&index=aps&linkCode=ur2&camp=1642&creative=6746'
+      url: 'http://www.amazon.fr/gp/search?ie=UTF8&keywords=%q&tag=604-21&index=aps&linkCode=ur2&camp=1642&creative=6746',
+	  json: 'http://completion.amazon.co.uk/search/complete?method=completion&q=%q&search-alias=aps&mkt=4'
     },
     ebay: {
-      url: 'http://shop.ebay.%ld/?_nkw=%q'
+      url: 'http://shop.ebay.%ld/?_nkw=%q',
+	  json: 'http://anywhere.ebay.com/services/suggest/?s=0&q=%q' // from firefoxes searchservice
     },
     yahoo: {
-      url: 'http://%ld.search.yahoo.com/search?p=%q&ei=UTF-8'
+      url: 'http://%ld.search.yahoo.com/search?p=%q&ei=UTF-8',
+	  json: false
     },
     bing: {
-      url: 'http://www.bing.com/search?q=%q&form=QBLH'
+      url: 'http://www.bing.com/search?q=%q&form=QBLH',
+	  json: 'http://api.bing.com/osjson.aspx?query=%q&form=OSDJAS'
     },
     wikipedia: {
-      url: 'http://%ls.wikipedia.org/w/index.php?search=%q'
+      url: 'http://fr.wikipedia.org/w/index.php?search=%q',
+	  json: 'http://fr.wikipedia.org/w/api.php?action=opensearch&search=%q'
+    },
+    wolframalpha: {
+      url: 'http://www.wolframalpha.com/input/?i=%q',
+	  json: false
     }
   });
   
