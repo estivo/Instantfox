be848ec9ad90dae2d6abdc6b4c99761b7306d173
nightwing <amirjanyan@gmail.com>
2011-06-16 18:33:34 +0500

    todo

--- plugins/firefox/todo.js
+++ plugins/firefox/todo.js
@@ -5,24 +5,40 @@ function makeReqAsync(href, callback) {
     req.onload= function(){callback(req.responseText);};
     req.send(null);
 }
+function processRequest(html){
+	var ei = html.lastIndexOf('</body>')
+	var si = html.indexOf('<body')
+	
+	var match = html.slice(0, si).match(/<title>(.*?)<\/title>/)
+	var title = match ? match[1]:''
+	
+	var bodyHTML = html.slice(si, ei+7)
+	var bi = bodyHTML.indexOf('>')
+	var attrText = bodyHTML.substring(5, bi)
+
+	
+	var el = content.document.body
+	el.innerHTML = bodyHTML
+	// update attributes
+	var attrs = el.attributes
+	for(var i = attrs.length; i--;)
+		el.removeAttribute(attrs[i].nodeName)
+	
+	var reg = /(\w+)(?:\s*=\s*(".*?"|'.*?'|[^'">\s]+))?/g;
+	var match, attrs = [];
+	while (match = reg.exec(attrText)) {	
+		var t2 = match[2] || ''
+		if(t2 && (t2[0]=='"' || t2[0]=="'"))
+			t2 = t2.slice(1, -1)
+		attrs.push([match[1], t2])
+	}
+	attrs.forEach(function(x)el.setAttribute(x[0],x[1]))
+	// update url	
+	content.document.title = title
+	content.history.replaceState(null, title, url)
+}
+
 
 content.document.body.style.opacity='0.5'
-makeReqAsync('http://en.wikipedia.org/wiki/H',function(x){
-	cb=x
-	var ei=x.lastIndexOf('</body>')
-	var si=x.indexOf('<body')
-
-	content.document.body.innerHTML=cb.slice(si,ei+7)
-	content.document.body.style.opacity='1'
-})
-
-
-
-
-
-content.history.replaceState
-
-var stateObj = { foo: "bar" };
-content.history.pushState
-content.document.title='poi'
-content.history.replaceState('', "page 2", "/op/777bar.html");
+url = 'http://www.google.com/search?q=hello'
+makeReqAsync(url, processRequest)
